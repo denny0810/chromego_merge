8a499f90d5865d015aaeb0d60a8001a5cc2d42b3
denny0810 <59128785+denny0810@users.noreply.github.com>
2025-02-18 15:26:57 +0800

    Update meta_merge.py
    ignore xray

--- meta_merge.py
+++ meta_merge.py
@@ -296,7 +296,7 @@ process_urls("./urls/hysteria_urls.txt", process_hysteria)
 process_urls("./urls/hysteria2_urls.txt", process_hysteria2)
 
 # 处理 xray URLs
-process_urls("./urls/xray_urls.txt", process_xray)
+# process_urls("./urls/xray_urls.txt", process_xray)
 
 # 读取普通的配置文件内容
 with open("./templates/clash_template.yaml", "r", encoding="utf-8") as file:
